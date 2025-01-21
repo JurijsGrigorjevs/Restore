789811d75909cf7a4072b68f9cfb45102ff42db3
yurijg@hotmail.com <yurijg@hotmail.com>
2025-01-21 10:12:27 +0000

    add Files

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,16 @@
+using API.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 
 var app = builder.Build();
@@ -10,5 +18,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.MapControllers();
+
+DbInitializer.InitDb(app);
 
 app.Run();
