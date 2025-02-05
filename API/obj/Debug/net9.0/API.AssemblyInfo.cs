5c1aac7c6d90ae26c4ef131b4efa6ddb3e6f4173
yurijg@hotmail.com <yurijg@hotmail.com>
2025-02-05 03:22:53 +0000

    add code

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4798640c5f6be626eecbd559d24f49305d9bc3f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13406f2d7d63a0a518a7827ba65f3ca4a6a9bf3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
